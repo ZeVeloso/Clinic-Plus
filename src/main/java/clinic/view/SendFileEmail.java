b3fd9f890fdb943a5b42261c0940aed96b151c4d
ZeVeloso <veloso.j04@gmail.com>
2021-09-30 17:20:21 +0000

    Backup Email

---
+++ src/main/java/clinic/view/SendFileEmail.java
@@ -0,0 +1,60 @@
+package clinic.view;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class SendFileEmail {
+
+    public static void main() {
+
+        Properties prop = new Properties();
+        prop.put("mail.smtp.auth", true);
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "25");
+        prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        final String username = "clinicaappmais@gmail.com";//
+        final String password = "ClinicaPaulo";
+        try{
+            Session session = Session.getInstance(prop, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(username, password);
+                }
+            });
+
+            // -- Create a new message --
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("clinicaappmais@gmail.com"));
+            message.setRecipients(
+                    Message.RecipientType.TO, InternetAddress.parse("veloso.tribos@gmail.com"));
+            message.setSubject("Backup DB");
+
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+            String formatDateTime = now.format(formatter);
+
+            String msg = "Database Backup " + formatDateTime;
+            MimeBodyPart attachmentBodyPart = new MimeBodyPart();
+            attachmentBodyPart.attachFile(new File("ClinicDB.db"));
+
+            MimeBodyPart mimeBodyPart = new MimeBodyPart();
+            mimeBodyPart.setContent(msg, "text/html");
+
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(mimeBodyPart);
+            multipart.addBodyPart(attachmentBodyPart);
+            message.setContent(multipart);
+
+            Transport.send(message);
+            System.out.println("Message sent.");
+        }catch (MessagingException | IOException e){
+            System.out.println("ERROOOOO: " + e);
+        }
+    }
+}
